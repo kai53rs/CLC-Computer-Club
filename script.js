96e0166f078a1b0589feacdbb8c98743a5560d8e
kai53rs <153867851+kai53rs@users.noreply.github.com>
2023-12-14 21:42:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// var what = document.getElementById("what");
+// var when=document.getElementById("when");
+// var whatDo = document.getElementById("whatDo");
+// var tempInfo = document.getElementById("tempInfo");
+// when.addEventListener("mouseover", function() {
+//   console.log("hello");
+//   tempInfo.innerHTML = "Every Thursday: 3:00pm-4:00pm, Room 260";
+// });
+// what.addEventListener("mouseover", function() {
+//   console.log("hello");
+//   tempInfo.innerHTML = "The Computer Club is an educational and fun group. The club explores both computer architecture and new application software. We try to help anyone with his or her computer problems. We also know quite a bit about programming and networking";
+// });
+// whatDo.addEventListener("mouseover", function() {
+// tempInfo.innerHTML = "Computer is a fun and active way to get involved in the school and coding. Some work on hardware and make arcade machines. Others work on software. in software you help prepare for hackathons and learn MSOE YOU MUST KNOW JAVA! we all help prepare and host hackathons.";
+// });
